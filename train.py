79586f99f57b03db6b25886332ab486bdefa16ad
Ula028 <utalalaj1@sheffield.ac.uk>
2021-03-12 19:17:51 +0000

    Create test and train datasets

--- train.py
+++ train.py
@@ -5,26 +5,28 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn import tree
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 def load_training_data():
-    dict_data = np.load('preprocessed_data/patches.npz')
+    dict_data = np.load('preprocessed_data/train_data.npz')
     patches_lr = dict_data['patches_lr']
     patches_hr = dict_data['patches_hr']
     return patches_lr, patches_hr
 
-def train_linear_regression(patches_lr, patches_hr):
-    lin_reg = LinearRegression().fit(patches_lr, patches_hr)
-    return lin_reg.score(patches_lr, patches_hr)
+def load_testing_data():
+    dict_data = np.load('preprocessed_data/test_data.npz')
+    patches_lr = dict_data['patches_lr']
+    patches_hr = dict_data['patches_hr']
+    return patches_lr, patches_hr
 
-def train_regression_tree(patches_lr, patches_hr):
-    reg_tree = tree.DecisionTreeRegressor().fit(patches_lr, patches_hr)
-    return reg_tree.score(patches_lr, patches_hr)
 
-def train_random_forest(patches_lr, patches_hr):
-    ran_forest = RandomForestRegressor(n_estimators=10).fit(patches_lr, patches_hr)
-    return ran_forest.score(patches_lr, patches_hr)
-    
-patches_lr, patches_hr = load_training_data()
-print("Linear regression:", train_linear_regression(patches_lr, patches_hr))
-print("Regression tree:", train_regression_tree(patches_lr, patches_hr))
-print("Random forest:", train_random_forest(patches_lr, patches_hr))
+train_lr, train_hr = load_training_data()
+test_lr, test_hr = load_testing_data()
+
+print("Training linear regression model...")
+lin_reg = LinearRegression().fit(train_lr, train_hr)
+prediction = lin_reg.predict(test_lr)
+rms = mean_squared_error(test_hr, prediction, squared=False)
+print("Score:", rms)
+# reg_tree = tree.DecisionTreeRegressor().fit(train_lr, train_hr)
+# ran_forest = RandomForestRegressor(n_estimators=10).fit(train_lr, train_hr)
