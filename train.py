0d20295599de13beb6f89ac76f2c9b65d8b4a416
Ula028 <utalalaj1@sheffield.ac.uk>
2021-04-26 21:05:50 +0100

    Add max_evals

--- train.py
+++ train.py
@@ -12,57 +12,52 @@ from hyperopt.pyll.stochastic import sample
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-import os
-os.environ['OMP_NUM_THREADS'] = "1"
 
 import utils
 
-def main():
-    print("Loading training data...")
-    train_lr, train_hr = utils.load_training_data()
+print("Loading training data...")
+train_lr, train_hr = utils.load_training_data()
 
-    # print("Training the imputer...")
-    # imputer = IterativeImputer(max_iter=10, random_state=0)
-    # imputer = imputer.fit(train_lr)
+# print("Training the imputer...")
+# imputer = IterativeImputer(max_iter=10, random_state=0)
+# imputer = imputer.fit(train_lr)
 
-    print("Training the model...")
-    # lin_reg = LinearRegression().fit(train_lr, train_hr)
-    # reg_tree = DecisionTreeRegressor(criterion='mse').fit(train_lr, train_hr)
+print("Training the model...")
+# lin_reg = LinearRegression().fit(train_lr, train_hr)
+# reg_tree = DecisionTreeRegressor(criterion='mse').fit(train_lr, train_hr)
 
-    n_estimators = sample(scope.int(hp.quniform('n_estimators', 5, 15, 1)))
-    max_depth = sample(scope.int(hp.quniform('max_depth', 10, 50, 1)))
-    max_features = hp.choice('max_features', ['auto', 'sqrt', 'log2'])
-    bootstrap = hp.choice('bootstrap', [True, False])
-    max_samples = hp.uniform('max_samples', 0, 1)
+n_estimators = sample(scope.int(hp.quniform('n_estimators', 5, 15, 1)))
+max_depth = sample(scope.int(hp.quniform('max_depth', 10, 50, 1)))
+max_features = hp.choice('max_features', ['auto', 'sqrt', 'log2'])
+bootstrap = hp.choice('bootstrap', [True, False])
 
-    estim = HyperoptEstimator(regressor=random_forest_regression('my_forest', n_estimators=n_estimators,
-                                                                max_depth=max_depth, max_features=max_features, bootstrap=bootstrap), trial_timeout=10800)
-    estim.fit(train_lr, train_hr)
+estim = HyperoptEstimator(regressor=random_forest_regression('my_forest', n_estimators=n_estimators,
+                                                             max_depth=max_depth, max_features=max_features, bootstrap=bootstrap),
+                          max_evals=20, use_partial_fit=True, trial_timeout=10800)
+estim.fit(train_lr, train_hr)
 
-    print("Loading testing data...")
-    test_lr, test_hr = utils.load_testing_data()
+print("Loading testing data...")
+test_lr, test_hr = utils.load_testing_data()
 
-    print(estim.score(test_lr, test_hr))
-    print(estim.best_model())
+print(estim.score(test_lr, test_hr))
+print(estim.best_model())
 
-    # print("Calculating normal distrubution...")
-    # mean = np.mean(train_lr, axis=0)
-    # covariance = np.cov(train_lr, rowvar=False)
+# print("Calculating the mean...")
+# mean = np.mean(train_lr, axis=0)
+# print("Calculating the variance...")
+# covariance = np.cov(train_lr, rowvar=False)
 
-    # save the model
-    with open('models/ran_forest_model.pickle', 'wb') as handle:
-        pickle.dump(estim.best_model(), handle)
+# # save the model
+# with open('models/lin_reg_model.pickle', 'wb') as handle:
+#     pickle.dump(lin_reg.best_model(), handle)
 
-    # # save the normal distribution
-    # with open('models/mean.pickle', 'wb') as handle:
-    #     pickle.dump(mean, handle)
+# # save the normal distribution
+# with open('models/mean.pickle', 'wb') as handle:
+#     pickle.dump(mean, handle)
 
-    # with open('models/covariance.pickle', 'wb') as handle:
-    #     pickle.dump(covariance, handle)
+# with open('models/covariance.pickle', 'wb') as handle:
+#     pickle.dump(covariance, handle)
 
-    # # save the imputer
-    # with open('models/imputer.pickle', 'wb') as handle:
-    #     pickle.dump(imputer, handle)
-
-if __name__ ==  '__main__':
-    main()
+# # save the imputer
+# with open('models/imputer.pickle', 'wb') as handle:
+#     pickle.dump(imputer, handle)
