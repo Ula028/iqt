fb0e91aa84d9cf7e78d832d744c1589b882c23a6
Ula028 <utalalaj1@sheffield.ac.uk>
2021-04-30 10:59:31 +0100

    Train ranadom forest with n_estimators=100

--- train.py
+++ train.py
@@ -12,8 +12,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from hyperopt.pyll.stochastic import sample
 from hyperopt.pyll.base import scope
-from hyperopt import hp
+from hyperopt import hp, tpe
 from hpsklearn import HyperoptEstimator, random_forest_regression, min_max_scaler
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pickle
 
@@ -28,7 +29,7 @@ def estimate_random_forest(train_lr, train_hr):
     max_features = hp.choice('max_features', ['auto', 'sqrt', 'log2'])
     bootstrap = hp.choice('bootstrap', [True, False])
 
-    estim = HyperoptEstimator(regressor=random_forest_regression(
+    estim = HyperoptEstimator(algo=tpe.suggest, regressor=random_forest_regression(
         'my_forest', n_estimators=n_estimators, max_depth=max_depth, max_features='sqrt', min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, bootstrap=False), preprocessing=[min_max_scaler('my_scaler')], max_evals=25, trial_timeout=10800)
     estim.fit(train_lr, train_hr)
 
@@ -59,14 +60,25 @@ def train_reg_tree(train_lr, train_hr):
 
 def train_ran_forest(train_lr, train_hr):
     print("Training the decision tree regressor...")
-    ran_forest = RandomForestRegressor(
-        max_depth=45, max_features='sqrt', n_estimators=14, n_jobs=-1, random_state=1).fit(train_lr, train_hr)
+    # ran_forest = RandomForestRegressor(n_estimators=10, max_depth=43, min_samples_split=11, min_samples_leaf=19, max_features='sqrt', bootstrap=False).fit(train_lr, train_hr)
+
+    ran_forest = RandomForestRegressor(n_estimators=100, max_depth=50, max_features='sqrt', bootstrap=True, n_jobs=-1).fit(train_lr, train_hr)
 
     # save the model
     with open('models/ran_forest_model.pickle', 'wb') as handle:
         pickle.dump(ran_forest, handle)
 
     return ran_forest
+
+def fit_scaler(train_lr):
+    scaler = MinMaxScaler()
+    scaler.fit(train_lr)
+    
+    # save the scaler
+    with open('models/min_max_scaler.pickle', 'wb') as handle:
+        pickle.dump(scaler, handle)
+        
+    return scaler
 
 
 def calculate_gaussian(train_lr):
@@ -87,5 +99,6 @@ def calculate_gaussian(train_lr):
 
 if __name__ == "__main__":
     train_lr, train_hr = utils.load_training_data()
-    best_model = estimate_random_forest(train_lr, train_hr)
-    print(best_model)
+    # scaler = utils.load_scaler()  
+    # scaler.transform(train_lr)
+    ran_forest = train_ran_forest(train_lr, train_hr)
