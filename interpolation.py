60b8c6bf15e022e5e7a6ff1e02fdf12d639f4234
Ula028 <utalalaj1@sheffield.ac.uk>
2021-04-30 12:58:11 +0100

    Interpolation for all subjects

--- interpolation.py
+++ interpolation.py
@@ -18,6 +18,8 @@ upsample_rate = 2  # the super-resolution factor (m in paper)
 
 subjects_test = ["175136", "180230", "468050",
                  "902242", "886674", "962058", "103212", "792867"]
+order = 1 # 0 - nearest interpolation, 1 - trilinear
+
 
 def linear_interpolation(subject):
     """ Computes DTIs from DWI upsampled by linear interpolation.
@@ -40,16 +42,16 @@ def linear_interpolation(subject):
     gtab = gradient_table(bvals, bvecs)
 
     # downsample the DWI
-    print("Downsampling the DWI...")
+    print("Downsampling the DWI for subject " + subject + "...")
     downsampled_voxsize = [i*upsample_rate for i in voxsize]
     data_lr, affine_lr = reslice(
         data_hr, affine_hr, voxsize, downsampled_voxsize)
 
     # upsample the DWI
-    print("Upsampling the DWI...")
+    print("Upsampling the DWI for subject " + subject + "...")
     upsampled_voxsize = [i/upsample_rate for i in downsampled_voxsize]
     data_hr, affine_hr = reslice(
-        data_lr, affine_lr, downsampled_voxsize, upsampled_voxsize)
+        data_lr, affine_lr, downsampled_voxsize, upsampled_voxsize, order=order)
 
     # get the brain mask for high-res DWI
     maskdata_hr, mask_hr = median_otsu(data_hr, vol_idx=range(10, 50), median_radius=3,
@@ -58,14 +60,14 @@ def linear_interpolation(subject):
     # compute DTI for the upsampled high-res DWI
     tenmodel = dti.TensorModel(gtab)
 
-    print("Fitting high resolution DTs...")
+    print("Fitting high resolution DTs for subject " + subject + "...")
     tenfit_hr = tenmodel.fit(maskdata_hr)
     quadratic_tensors_hr = tenfit_hr.quadratic_form
     evals_hr = tenfit_hr.evals
     evecs_hr = tenfit_hr.evecs
     print("High resolutions tensors:", quadratic_tensors_hr.shape)
     # save DTIs, eigenvectors, eigenvalues and mask to a file
-    filename = "reconstructed/inter" + subject + "tensors.npz"
+    filename = "reconstructed/" + subject + "inter_tensors.npz"
     np.savez_compressed(filename, tensors_hr=quadratic_tensors_hr, mask_hr=mask_hr,
                         evals_hr=evals_hr, evecs_hr=evecs_hr)
 
@@ -74,16 +76,3 @@ def linear_interpolation(subject):
 
 for subject in subjects_test:
     tensors_interpolated = linear_interpolation(subject)
-
-# load previously fitted DTIs
-tensor_file_hr = np.load("preprocessed_data/" + subject + "tensors_hr.npz")
-tensors_hr = tensor_file_hr['tensors_hr']
-
-# cast to common size if sizes different
-new_size = tensors_interpolated.shape
-if new_size != tensors_hr.shape:
-    original_hr = tensors_hr[:new_size[0], :new_size[1], :new_size[2]]
-
-# calculate rmse
-rmse = mean_squared_error(original_hr.flatten(),
-                          tensors_interpolated.flatten(), squared=False)
