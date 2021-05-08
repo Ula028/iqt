1b3830c0560660851ebfb05e2e52f75af289d470
Ula028 <utalalaj1@sheffield.ac.uk>
2021-05-08 23:21:03 +0100

    Change models used

--- create_patch_pair_grid.py
+++ create_patch_pair_grid.py
@@ -10,10 +10,12 @@ edge = 2*n + 1
 
 patches_lr, patches_hr = utils.load_training_data(50)
 
-# low resolution patch
+# get index
 s = patches_lr.shape
 index = np.random.choice(s[0])
 print(index)
+
+# low resolution patch
 patch = patches_lr[index, :]
 patch = np.reshape(patch, (edge, edge, edge, 6))
 new_patch = np.zeros((edge, edge, edge, 3, 3))
@@ -43,7 +45,7 @@ cfa /= cfa.max()
 scene.add(actor.tensor_slicer(evals, evecs, scalar_colors=cfa, sphere=sphere,
                                 scale=0.3))
 
-path = 'images/patches/' + str(index) + 'patch_lr.png'
+path = 'images/patches/grid' + str(index) + 'patch_lr.png'
 window.record(scene, n_frames=1, out_path=path,
                 size=(1200, 1200))
 
@@ -79,13 +81,13 @@ cfa /= cfa.max()
 scene.add(actor.tensor_slicer(evals, evecs, scalar_colors=cfa, sphere=sphere,
                                 scale=0.3))
 
-path = 'images/patches/' + str(index) + 'patch_hr.png'
+path = 'images/patches/grid' + str(index) + 'patch_hr.png'
 window.record(scene, n_frames=1, out_path=path,
                 size=(1200, 1200))
 
 
 # linear regression
-model = utils.load_linear_model()
+model = utils.load_linear_model(10)
 patch = model.predict(patches_lr[index, :].flatten().reshape(1, -1))
 patch = np.reshape(patch, (m, m, m, 6))
 new_patch = np.zeros((m, m, m, 3, 3))
@@ -115,13 +117,13 @@ cfa /= cfa.max()
 scene.add(actor.tensor_slicer(evals, evecs, scalar_colors=cfa, sphere=sphere,
                                 scale=0.3))
 
-path = 'images/patches/' + str(index) + 'patch_rec_lin_reg.png'
+path = 'images/patches/grid' + str(index) + 'patch_rec_lin_reg.png'
 window.record(scene, n_frames=1, out_path=path,
                 size=(1200, 1200))
 
 
 # regression tree
-model = utils.load_reg_tree_model()
+model = utils.load_reg_tree_model(10)
 patch = model.predict(patches_lr[index, :].flatten().reshape(1, -1))
 patch = np.reshape(patch, (m, m, m, 6))
 new_patch = np.zeros((m, m, m, 3, 3))
@@ -151,13 +153,13 @@ cfa /= cfa.max()
 scene.add(actor.tensor_slicer(evals, evecs, scalar_colors=cfa, sphere=sphere,
                                 scale=0.3))
 
-path = 'images/patches/' + str(index) + 'patch_rec_reg_tree.png'
+path = 'images/patches/grid' + str(index) + 'patch_rec_reg_tree.png'
 window.record(scene, n_frames=1, out_path=path,
                 size=(1200, 1200))
 
 
 # random forest
-model = utils.load_rand_forest_model()
+model = utils.load_rand_forest_model(10)
 patch = model.predict(patches_lr[index, :].flatten().reshape(1, -1))
 patch = np.reshape(patch, (m, m, m, 6))
 new_patch = np.zeros((m, m, m, 3, 3))
@@ -187,6 +189,6 @@ cfa /= cfa.max()
 scene.add(actor.tensor_slicer(evals, evecs, scalar_colors=cfa, sphere=sphere,
                                 scale=0.3))
 
-path = 'images/patches/' + str(index) + 'patch_rec_ran_forest.png'
+path = 'images/patches/grid' + str(index) + 'patch_rec_ran_forest.png'
 window.record(scene, n_frames=1, out_path=path,
                 size=(1200, 1200))
